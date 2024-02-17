46631fd04ca96f5898307434abbe59209cd3dfde
Nijat Muradov <live4ever4@gmail.com>
2024-02-17 13:47:59 +0400

    Edit style, html, js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,117 @@
-const ROOT_WRAPPER = document.querySelector('.wrapper');
-const ROOT_HEADER = document.querySelector('.header');
-const ROOT_STORE_CARD = document.querySelector('.store-card-img');
-const ROOT_LOADER = document.querySelector('.loader');
-const ROOT_PRODUCTS = document.querySelector('.products-items');
-const ROOT_ERROR = document.querySelector('.error');
+// Глобальные переменные
+const ROOT_PRODUCTS = document.getElementById('root-products');
+const ROOT_HEADER = document.getElementById('root-header');
+const ROOT_STORE_CARD = document.getElementById('root-store-card');
+const ROOT_LOADER = document.getElementById('root-loader');
+const ROOT_ERROR = document.getElementById('root-error');
+
+// Класс LocalStorageUtil
+class LocalStorageUtil {
+    constructor() {
+        this.keyName = 'products';
+    }
+
+    getProducts() {
+        const products = localStorage.getItem(this.keyName);
+        if (products !== null) {
+            return JSON.parse(products);
+        }
+        else {
+            return [];
+        }
+    }
+
+    putProducts(id) {
+        const products = this.getProducts();
+        
+        let pushProduct = false;
+        const index = products.indexOf(id);
+        if (index === -1) {
+            products.push(id);
+            pushProduct = true;
+        } else {
+            products.splice(index, 1);
+        }
+
+        localStorage.setItem(this.keyName, JSON.stringify(products));
+        return { pushProduct, products };
+    }
+}
+
+// Класс StoreCard
+class StoreCard {
+    handlerClear() {
+        ROOT_STORE_CARD.innerHTML = '';
+    }
+
+    render() {
+        const products = localStorageUtil.getProducts();
+        let htmlCatalog = '';
+        let sumCatalog = 0;
+
+        // Здесь должна быть логика формирования HTML для корзины
+        // и вычисление общей суммы покупок
+
+        ROOT_STORE_CARD.innerHTML = htmlCatalog;
+    }
+}
+
+// Класс Header
+class Header {
+    handlerOpenStoreCardPage() {
+        storeCardPage.render();
+    }
+
+    render(count) {
+        // Формирование HTML для верхней части страницы
+        ROOT_HEADER.innerHTML = `<img class="store-card-img" src="img/cart.svg" alt=""> ${count}`;
+    }
+}
+
+// Класс Products
+class Products {
+    constructor() {
+        this.classNameActive = 'active';
+        this.labelAdd = 'Добавить в корзину';
+        this.labelRemove = 'Удалить из корзины';
+    }
+
+    handlerSetLocalStorage(button, id) {
+        const { pushProduct, products } = localStorageUtil.putProducts(id);
+        const buttonText = pushProduct ? this.labelRemove : this.labelAdd;
+        const buttonClass = pushProduct ? this.classNameActive : '';
+
+        button.innerText = buttonText;
+        button.classList.toggle(this.classNameActive, pushProduct);
+
+        // Обновление счетчика в шапке страницы
+        headerPage.render(products.length);
+    }
+
+    render() {
+        const products = localStorageUtil.getProducts();
+        let htmlCatalog = '';
+
+        CATALOG.forEach(product => {
+            const { id, title, price, description, image } = product;
+            const productInStore = products.includes(id);
+            const buttonText = productInStore ? this.labelRemove : this.labelAdd;
+            const buttonClass = productInStore ? this.classNameActive : 'no-active';
+
+            htmlCatalog += `
+                <div class="product">
+                    <img src="${image}" alt="${title}" />
+                    <div class="product-title"><b>${title}</b></div>
+                    <div class="product-price">Price: ${price}$</div>
+                    <div class="product-description">${description}</div>
+                    <button class="${buttonClass}" onclick="productsPage.handlerSetLocalStorage(this, ${id})">${buttonText}</button>
+                </div>
+            `;
+        });
+
+        ROOT_PRODUCTS.innerHTML = htmlCatalog;
+    }
+}
 
 class LoaderPage {
     //После задержки, вызывается  для скрытия индикатора загрузки
@@ -17,159 +125,36 @@ class LoaderPage {
     // }
 }
 
-class LocalStorageUtil {
-    //Строка, представляющая ключ для доступа к данным в локальном хранилище.
-    keyName = null;
 
-    constructor() {
-        //Устанавливает ключ 'products' для хранения данных в локальном хранилище.
-        this.keyName = 'products';
-    }
-
-    getProducts() {
-        //Получает продукты из локального хранилища по установленному ключу.
-        const products = localStorage.getItem(this.keyName);
-        if (products !== null) {
-            return JSON.parse(products);
-        }
-        else {
-            return [];
-        }
-    };
-
-    putProducts(id) {
-        // Извлекает текущий список продуктов из локального хранилища с помощью метода getProducts().
-        const products = this.getProducts();
-
-        // Инициализирует переменную pushProduct значением false (предполагая, что продукт не был добавлен).
-        let pushProduct = false;
-
-        // Проверяет индекс продукта в текущем списке.
-        // Если продукт с указанным идентификатором отсутствует, то добавляет его в список и устанавливает pushProduct в true.
-        // Если продукт уже присутствует в списке, удаляет его с помощью метода splice.
-        const index = products.indexOf(id);
-        if (index === -1) {
-            products.push(id);
-            pushProduct = true;
-        }
-        else {
-            products.slice(index, 1);
-        }
-
-        // Обновляет локальное хранилище с обновленным списком продуктов в формате JSON.
-        // Возвращает объект с двумя свойствами: pushProduct (логическое значение, указывающее, был ли продукт добавлен) и products (обновленный массив продуктов).
-        localStorage.setItem(this.keyName, JSON.stringify(products));
-        return { pushProduct, products };
-    };
-}
-
-class StoreCard {
-    handlerClear() {
-        //Обращается к глобальному элементу ROOT_STORE_CARD и устанавливает его содержимое в пустую строку, тем самым очищая корзину на веб-странице
-        ROOT_STORE_CARD.innerHTML = '';
-    };
-
-    render() {
-        //Получает текущий список продуктов из локального хранилища с помощью localStorageUtil.getProducts().
-        const products = localStorageObj.getProducts();
-
-        //Инициализирует переменные htmlCatalog и sumCatalog для хранения HTML-разметки продуктов в корзине и общей стоимости покупок соответственно.
-        let htmlCatalog = '';
-        let sumCatalog = 0;
-
-        //Итерирует по массиву CATALOG, проверяя, присутствует ли продукт в корзине (поиск по идентификатору). 
-        //Если присутствует, формирует HTML-разметку для продукта в корзине и увеличивает сумму.
-        //Собирает весь HTML-контейнер для корзины, включая таблицу с продуктами и общей суммой.
-        //Обновляет содержимое элемента с идентификатором ROOT_STORE_CARD на веб-странице
-        ROOT_STORE_CARD.innerHTML = htmlCatalog;
-
-    };
-}
-
-class Header {
-    handlerOpenStoreCardPage() {
-        //Вызывает метод render у объекта storeCardPage, что приводит к обновлению содержимого корзины на веб-странице
-        storeCardObj.render();
-    };
-
-    render(count) {
-        //Формирует HTML-разметку для верхней части страницы, включая счетчик продуктов и кнопку для открытия корзины. 
-        //Обновляет содержимое элемента с идентификатором ROOT_HEADER на веб-странице.
-        //ROOT_HEADER.innerHTML = `В корзине ${count} товаров`;
-    };
-}
-
-class Products {
-    classNameActive = null; //Строка, представляющая класс для стилизации продукта, добавленного в корзину
-    labelAdd = null; //Строка, представляющая текст кнопки "Добавить в корзину"
-    labelRemove = null; //Строка, представляющая текст кнопки "Удалить из корзины" 
-
-    constructor() {
-        this.classNameActive = 'active';
-        this.labelAdd = 'В корзину';
-        this.labelRemove = 'Удалить из корзины';
-    }
-
-    // <div class="products-item"></div>
-    // <div class="products-item-to-basket-button">В корзину</div>
-
-    handlerSetLocalStorage(element, id) {
-        //Вызывает метод putProducts у объекта localStorageUtil для добавления или удаления продукта из корзины.
-        const { pushProduct, products } = localStorageObj.putProducts();
-        const buttonText = pushProduct ? this.labelRemove : this.labelAdd;
-        const btnClass = pushProduct ? this.classNameActive : '';
-
-        element.innerText = buttonText;
-        element.classList.toggle(this.classNameActive, pushProduct);
-
-        //Обновляет отображение кнопки на веб-странице и вызывает метод render у объекта headerPage для обновления счетчика продуктов в верхней части страницы.
-        headerObj.render(products.length);
-    };
-
-    render() {
-        //Получает список продуктов из локального хранилища.
-        const products = localStorageObj.getProducts();
-
-        //Инициализирует переменную htmlCatalog для хранения HTML-разметки продуктов.
-        let htmlCatalog = null;
-
-        //Итерирует по массиву CATALOG, формируя HTML-разметку для каждого продукта в зависимости от того, добавлен ли продукт в корзину или нет.
-        CATALOG.forEach(productItem => {
-            const { id, title, price, description, image } = productItem;
-            const inBasket = products.includes(id);
-            const buttonText = inBasket ? this.labelRemove : this.labelAdd;
-            const btnClass = inBasket ? this.classNameActive : '';
-
-            htmlCatalog += `
-                <div class="product-item">
-                    <img class="product-item-img" src="${image}" alt="${title}" />
-                    <div class="product-item-title">${title}</div>
-                    <div class="product-item-price">Price: ${price}$</div>
-                    <div class="product-item-description">${description}</div>
-                    <button class="${btnClass}" onclick="productsObj.handlerSetLocalStorage(this, ${id})">${buttonText}</button>
-                </div> 
-            `;
-        });
-
-        //Обновляет содержимое контейнера продуктов на веб-странице с учетом новой HTML-разметки
-        ROOT_PRODUCTS.innerHTML = htmlCatalog;
-    };
-}
-
-const loaderPageObj = new LoaderPage();
-const localStorageObj = new LocalStorageUtil();
-const storeCardObj = new StoreCard();
-const headerObj = new Header();
-const productsObj = new Products();
+// Инициализация данных
+const localStorageUtil = new LocalStorageUtil();
+const storeCardPage = new StoreCard();
+const headerPage = new Header();
+const productsPage = new Products();
+const loaderPage = new LoaderPage();
 let CATALOG = [];
 
-fetch('https://fakestoreapi.com/products').then(response => response.json()).then(data => {
-    CATALOG = data;
+const errorPage = {
+    render() {
+        ROOT_ERROR.innerHTML = 'Ошибка загрузки данных';
+    }
+};
 
-    setTimeout(() => {
-        loaderPageObj.handlerClear();
-        const productsStore = localStorageObj.getProducts();
-        headerObj.render(productsStore.length);
-        productsObj.render();
-    }, 1000)
-});
+
+ROOT_LOADER.style.display = 'block';
+
+fetch('https://fakestoreapi.com/products').then(response => response.json()).then(data => {
+        CATALOG = data;
+
+        setTimeout(() => {
+            loaderPage.handlerClear();
+            const productsStore = localStorageUtil.getProducts();
+            headerPage.render(productsStore.length);
+            productsPage.render();
+        }, 500);
+    })
+    .catch(error => {
+        loaderPage.handleClear();
+        errorPage.render();
+        console.error('Ошибка загрузки данных:', error);
+    });
